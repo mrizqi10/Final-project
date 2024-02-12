e72208d8d11d80105071db8062e05e1fd08268ed
rosan <awassb75@gmail.com>
2024-02-12 08:48:10 +0700

    p

--- app/src/main/java/com/kodulersfittasa/Onboarding1.kt
+++ app/src/main/java/com/kodulersfittasa/Onboarding1.kt
@@ -3,7 +3,7 @@ package com.kodulersfittasa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class onboarding1 : AppCompatActivity() {
+class Onboarding1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding1)
