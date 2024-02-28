5d2fbde6709c3c15528ec4f6e48d34698b5c9ac1
mrizqi10 <rizq2110@gmail.com>
2024-02-28 09:51:21 +0700

    "nambahin codingan on boarding"

--- app/src/main/java/com/kodulersfittasa/Onboarding1.kt
+++ app/src/main/java/com/kodulersfittasa/Onboarding1.kt
@@ -1,11 +1,22 @@
 package com.kodulersfittasa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kodulersfittasa.databinding.Onboarding1Binding
 
 class Onboarding1 : AppCompatActivity() {
+
+    private lateinit var binding: Onboarding1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.onboarding1)
+        binding = Onboarding1Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button1.setOnClickListener{
+            startActivity(Intent(this, onboarding2::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/kodulersfittasa/onboarding2.kt
+++ app/src/main/java/com/kodulersfittasa/onboarding2.kt
@@ -1,11 +1,23 @@
 package com.kodulersfittasa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kodulersfittasa.databinding.Onboarding1Binding
+import com.kodulersfittasa.databinding.Onboarding2Binding
 
 class onboarding2 : AppCompatActivity() {
+
+    private lateinit var binding: Onboarding2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.onboarding2)
+        binding = Onboarding2Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button2.setOnClickListener{
+            startActivity(Intent(this, onboarding3::class.java))
+        }
+
     }
 }
