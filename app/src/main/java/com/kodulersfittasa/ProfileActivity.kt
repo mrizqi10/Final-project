5f6914c9f1ae7477affbf577bd5ddd276bf8c84d
Fadlan Hidayat <mfh.fadlanhidayat@gmail.com>
2024-03-04 21:24:16 +0700

    p

--- app/src/main/java/com/kodulersfittasa/ProfileActivity.kt
+++ app/src/main/java/com/kodulersfittasa/ProfileActivity.kt
@@ -7,5 +7,11 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile2)
+        val fat = intent.getFloatExtra("fat", 0f)
+        val carbs = intent.getFloatExtra("carbs", 0f)
+        val calories = intent.getFloatExtra("calories", 0f)
+        val protein = intent.getFloatExtra("protein", 0f)
+
+// Now you can use these values in your ProfileActivity
     }
 }
--- app/src/main/java/com/kodulersfittasa/detail_makanan.kt
+++ app/src/main/java/com/kodulersfittasa/detail_makanan.kt
@@ -1,11 +1,24 @@
 package com.kodulersfittasa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class detail_makanan : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_makanan)
+
+        val intent = Intent(this, ProfileActivity::class.java)
+
+        intent.putExtra("fat", 65)
+        intent.putExtra("carbs", 84)
+        intent.putExtra("calories", 190)
+        intent.putExtra("protein", 87)
+        startActivity(intent)
+
+
     }
 }
