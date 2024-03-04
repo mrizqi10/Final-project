61b6f8334d926f6faf72922929a59f79acaa53b8
mrizqi10 <rizq2110@gmail.com>
2024-03-04 21:41:26 +0700

    "nambahin codingan on boarding"

--- app/src/main/java/com/kodulersfittasa/before_register.kt
+++ app/src/main/java/com/kodulersfittasa/before_register.kt
@@ -1,11 +1,28 @@
 package com.kodulersfittasa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kodulersfittasa.databinding.ActivityBeforeRegisterBinding
+import com.kodulersfittasa.databinding.Onboarding2Binding
+import kotlin.math.sign
 
 class before_register : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBeforeRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_before_register)
+        binding = ActivityBeforeRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonLogin.setOnClickListener{
+            startActivity(Intent(this, login::class.java))
+        }
+
+        binding.buttonSignIn.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+
     }
 }
